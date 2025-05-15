129b2debc1c76d7cca43e848abc12baf9f2d5f03
Endalkachewe <133228700+Endalkachewe@users.noreply.github.com>
2025-05-15 15:36:41 -0800

    Netflix rows

--- react-app-netflix/src/Page/Home/Home.jsx
+++ react-app-netflix/src/Page/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
 import Banner from '../../Components/Banner/Banner'
+import RowList from '../../Components/Rows/RowsList/RowList'
 function Home() {
   return (
     <div className='app'>
       <Header />
-      <Banner/>
+      <Banner />
+      <RowList/>
       <Footer/>
           {/* <Foo/> */}
     </div>
